39596d1e45c85f4cdf7dedfdcdd7cd34fc2f0cf0
zorkb <zachbuchs@gmail.com>
2021-11-10 15:20:41 -0800

    footer and header components in

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return ( 
+        <header class="vertical-center">
+            <div>
+            <h1><a href="index.html">Monument</a></h1>
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
