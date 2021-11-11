bf02cb91f3e41118d7b952aa42c1cd88994b5399
zorkb <zachbuchs@gmail.com>
2021-11-10 16:43:11 -0800

    Made article container, cleared out html from app.js

---
+++ src/ArticleContainer.js
@@ -0,0 +1,39 @@
+
+class ArticleContainer extends React.Component {
+
+    state = {
+
+    articles: [
+        {
+        id: "1",
+        image: "images/article_1.jpg",
+        title: "Issue Twenty - The Southwest",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptate eos nisi necessitatibus aperiam dolorem ut eveniet quae dolores quisquam nulla, cumque tenetur blanditiis modi tempora excepturi error veniam suscipit dicta praesentium reiciendis illo obcaecati odit inventore minima. Pariatur facilis, excepturi! Facilis cumque dicta reiciendis nam accusantium cum pariatur sunt accusamus voluptas eaque sapiente totam dolorum nulla eligendi molestiae, quidem odit repellat temporibus quae ipsa aut placeat modi sint doloribus. Ipsum nisi veritatis, facere doloribus. Nihil veritatis corporis eaque odio.",
+        },
+        {
+        id: "2",
+        image: "images/article_2.jpg",
+        title: "Issue Nineteen - Camping",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptate eos nisi necessitatibus aperiam dolorem ut eveniet quae dolores quisquam nulla, cumque tenetur blanditiis modi tempora excepturi error veniam suscipit dicta praesentium reiciendis illo obcaecati odit inventore minima. Pariatur facilis, excepturi! Facilis cumque dicta reiciendis nam accusantium cum pariatur sunt accusamus voluptas eaque sapiente totam dolorum nulla eligendi molestiae, quidem odit repellat temporibus quae ipsa aut placeat modi sint doloribus. Ipsum nisi veritatis, facere doloribus. Nihil veritatis corporis eaque odio.",
+        },
+        {
+        id: "3",
+        image: "images/article_3.jpg",
+        title: "Issue Eighteen - Food",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptate eos nisi necessitatibus aperiam dolorem ut eveniet quae dolores quisquam nulla, cumque tenetur blanditiis modi tempora excepturi error veniam suscipit dicta praesentium reiciendis illo obcaecati odit inventore minima. Pariatur facilis, excepturi! Facilis cumque dicta reiciendis nam accusantium cum pariatur sunt accusamus voluptas eaque sapiente totam dolorum nulla eligendi molestiae, quidem odit repellat temporibus quae ipsa aut placeat modi sint doloribus. Ipsum nisi veritatis, facere doloribus. Nihil veritatis corporis eaque odio.",
+        },
+    ]
+    }
+
+    render() {
+    return (
+        <div>
+        <ArticleList 
+            articles={this.state.articles}
+        />
+        </div>
+    )
+    }
+}
+
+export default ArticleContainer;
